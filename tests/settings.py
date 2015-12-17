8a62af4ae91acb581d765084733e3ceae4c9c005
Florent Messa <florent.messa@gmail.com>
2015-12-17 10:40:53 +0100

    Write on stdout

--- tests/settings.py
+++ tests/settings.py
@@ -3,10 +3,10 @@ import sys
 try:
     from .credentials import *  # noqa
 except ImportError:
-    sys.stderr.write('Error: Can\'t find the file credentials.py')
+    sys.stdout.write('Error: Can\'t find the file credentials.py')
 
-    MANGOPAY_CLIENT_ID = None
-    MANGOPAY_PASSPHRASE = None
+    MANGOPAY_CLIENT_ID = 'chouette'
+    MANGOPAY_PASSPHRASE = 'bar'
     MANGOPAY_API_URL = 'https://api.mangopay.com/v2/'
     MANGOPAY_API_SANDBOX_URL = 'https://api.sandbox.mangopay.com/v2/'
     MANGOPAY_USE_SANDBOX = True
